7457e89ad72a9815a24ee3386188f3f589bed2aa
Сакелов Эмир <sakelov99@gmail.com>
2022-02-02 09:46:57 +0600

    first commit movie_rest

---
+++ movies/service.py
@@ -0,0 +1,31 @@
+from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Movie
+
+
+class PaginationMovies(PageNumberPagination):
+    page_size = 2
+    max_page_size = 1000
+
+
+def get_client_ip(request):
+    """Получаем IP пользователя"""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
+class MovieFilter(filters.FilterSet):
+    genres = CharFilterInFilter(field_name='genres__name', lookup_expr='in')
+    year = filters.RangeFilter()
+
+    class Meta:
+        model = Movie
+        fields = ['genres', 'year']
